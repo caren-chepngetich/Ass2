64e0b6ff25bdf53820686858c993074df80827cb
Student <Caren-Chepngetich>
2024-03-25 08:11:26 +0300

    added files

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,38 @@
+fun main(){
+  words("Barnie bakes brown bagels and buns")
+    my(20,30,40)
+    volume(21)
+}
+//Question 1
+fun words(name:String){
+var words.split[b]
+    println("Barnie bakes brown bagels and buns")
+
+}
+
+//Question 2
+
+fun my(num1:Int,num2:Int,num3:Int):Int{
+   var sum = num1 + num2 + num3
+   var count = (sum.)
+    var average = (sum.a)
+}
+
+//Question 3
+
+fun volume(num:Int){
+    var radius = 3.14159
+println(volume)
+}
+
+//Questin 4
+fun isPalindrome(word:String):Boolean{
+    var word = "Kayak"
+    if word is a palindrome{
+        println(true)
+
+    }
+    else{
+        println(false)
+    }
+}
